97d8272debc82cc4b8552fe074bb63b25f4b0142
tianzhonghai <tianzhonghai@ymatou.com>
2017-05-02 12:02:04 +0800

    增加静态文件

--- src/main/java/com/tim/gaea2/web/controllers/UserController.java
+++ src/main/java/com/tim/gaea2/web/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -51,6 +52,13 @@ public class UserController {
 
     @RequestMapping("/view")
     public String view(int id,Model model){
+        UserVO userVO = userInfoService.getUserVoByUserId(id);
+        model.addAttribute("User",userVO);
+        return "users/view";
+    }
+
+    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
+    public String detail(@PathVariable int id, Model model){
         UserVO userVO = userInfoService.getUserVoByUserId(id);
         model.addAttribute("User",userVO);
         return "users/view";
