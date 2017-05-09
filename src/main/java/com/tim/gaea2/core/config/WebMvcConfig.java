3174dae263a6e96ca0b39530ba126d69ace10d75
tianzhonghai <tianzhonghai@ymatou.com>
2017-05-09 16:34:57 +0800

    授权相关的配置类

--- src/main/java/com/tim/gaea2/core/config/WebMvcConfig.java
+++ src/main/java/com/tim/gaea2/core/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.tim.gaea2.core.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
  * Created by tianzhonghai on 2017/5/4.
  */
 @Configuration
+@ComponentScan("com.tim.gaea2.web.controllers")
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
