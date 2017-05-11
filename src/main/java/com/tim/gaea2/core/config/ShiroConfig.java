c464ac38a1b211d84b7e4aad0dc9a0b5fc401a73
tianzhonghai <tianzhonghai@outlook.com>
2017-05-11 16:30:30 +0800

    Update ShiroConfig.java

--- src/main/java/com/tim/gaea2/core/config/ShiroConfig.java
+++ src/main/java/com/tim/gaea2/core/config/ShiroConfig.java
@@ -64,7 +64,138 @@ public class ShiroConfig {
         return daap;
     }
 
+    @Bean(name = "securityManager")package com.tim.gaea2.core.config;
+
+import com.tim.gaea2.core.shiro.CustomAuthorizingRealm;
+import com.tim.gaea2.core.shiro.CustomHashedCredentialsMatcher;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by tianzhonghai on 2017/5/9.
+ */
+@Configuration
+public class ShiroConfig {
+    private static Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+
+    @Bean(name = "ShiroRealm")
+    public CustomAuthorizingRealm getShiroRealm(@Qualifier("credentialsMatcher") CredentialsMatcher matcher) {
+        CustomAuthorizingRealm shiroRealm = new CustomAuthorizingRealm();
+        shiroRealm.setCredentialsMatcher(matcher);
+        return shiroRealm;
+    }
+
+    @Bean(name = "memoryCacheManager")
+    public CacheManager getMemoryCacheManager() {
+        MemoryConstrainedCacheManager manager = new MemoryConstrainedCacheManager();
+        return manager;
+    }
+
+    @Bean(name="credentialsMatcher")
+    public CustomHashedCredentialsMatcher getCredentialsMatcher(){
+        CustomHashedCredentialsMatcher platFormCredentialsMatcher = new CustomHashedCredentialsMatcher();
+        platFormCredentialsMatcher.setHashAlgorithmName("MD5");
+        platFormCredentialsMatcher.setHashIterations(1);
+        platFormCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+        return platFormCredentialsMatcher;
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator daap = new DefaultAdvisorAutoProxyCreator();
+        daap.setProxyTargetClass(true);
+        return daap;
+    }
+
     @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("ShiroRealm") CustomAuthorizingRealm shiroRealm) {
+        DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
+        dwsm.setRealm(shiroRealm);
+        dwsm.setCacheManager(getMemoryCacheManager());
+        return dwsm;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor getAuthorizationAttributeSourceAdvisor(@Qualifier("securityManager")DefaultWebSecurityManager dwsm) {
+        AuthorizationAttributeSourceAdvisor aasa = new AuthorizationAttributeSourceAdvisor();
+        aasa.setSecurityManager(dwsm);
+        return new AuthorizationAttributeSourceAdvisor();
+    }
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager")DefaultWebSecurityManager dwsm) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(dwsm);
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setSuccessUrl("/home/index");
+        //shiroFilterFactoryBean.setUnauthorizedUrl("/login");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403"); //认证未通过的页面，即未经授权页面
+
+        //Map<String, Filter> filters = new LinkedHashMap<>();
+        //filters.put("authc", new FormAuthenticationFilter());
+        //filters.put("rbac", new RbacAuthenticationFilter());
+        //filters.put("roles", new RolesAuthorizationFilter());
+        //shiroFilterFactoryBean.setFilters(filters);
+
+        loadShiroFilterChain(shiroFilterFactoryBean);
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * FilterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
+        //该值缺省为false,表示生命周期由SpringApplicationContext管理,设置为true则表示由ServletContainer管理
+        filterRegistration.addInitParameter("targetFilterLifecycle", "true");
+        filterRegistration.setEnabled(true);
+        filterRegistration.addUrlPatterns("/*");
+        filterRegistration.setDispatcherTypes(DispatcherType.REQUEST);
+        return filterRegistration;
+    }
+
+    private void loadShiroFilterChain(ShiroFilterFactoryBean shiroFilterFactoryBean){
+        /////////////////////// 下面这些规则配置最好配置到配置文件中 ///////////////////////
+        ///Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+        // authc：该过滤器下的页面必须验证后才能访问，它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter
+        filterChainDefinitionMap.put("/user/**", "authc");// 这里为了测试，只限制/user，实际开发中请修改为具体拦截的请求规则
+        // anon：它对应的过滤器里面是空的,什么都没做
+        //logger.info("##################从数据库读取权限规则，加载到shiroFilter中##################");
+        //filterChainDefinitionMap.put("/user/edit/**", "authc,perms[user:edit]");// 这里为了测试，固定写死的值，也可以从数据库或其他配置中读取
+
+        filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/**", "authc");//anon 可以理解为不拦截
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+    }
+}
+
     public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("ShiroRealm") CustomAuthorizingRealm shiroRealm) {
         DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
         dwsm.setRealm(shiroRealm);
