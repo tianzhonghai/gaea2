f1f3990b8977f1f479580ea8a4aee9a9553afbee
tianzhonghai <tianzhonghai@outlook.com>
2017-06-24 21:08:48 +0800

    更新索引对象

--- src/main/java/com/tim/gaea2/web/models/UserModel.java
+++ src/main/java/com/tim/gaea2/web/models/UserModel.java
@@ -1,10 +1,15 @@
 package com.tim.gaea2.web.models;
 
+import java.util.Date;
+
 /**
  * Created by tianzhonghai on 2017/4/1.
  */
 public class UserModel {
     private String username;
+    private String password;
+    private String state;
+    private Date createTime;
 
     public String getUsername() {
         return username;
@@ -14,13 +19,27 @@ public class UserModel {
         this.username = username;
     }
 
-    private String password;
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
     }
 }
